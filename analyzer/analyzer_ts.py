b3c7e4405aff2abac9c0d7c1c4527020e7b289aa
yevgenin <yevgeni@aperio-systems.com>
2017-06-13 09:57:42 +0300

    some updates

--- analyzer/analyzer_ts.py
+++ analyzer/analyzer_ts.py
@@ -32,7 +32,8 @@ class Analyzer(PredixWrap):
     async def main_async(self):
         await self.load_data()
         await self.insert_bad_data()
-        await self.compute_dq()
+        if self.config.do_compute:
+            await self.compute_dq()
         await self.show()
         await self.start_server()
 
@@ -62,7 +63,10 @@ class Analyzer(PredixWrap):
         else:
             return model.maparams
 
-    def get_vals(self, t):
+    async def get_vals(self, t):
+        if self.config.compute_live:
+            return await self.get_vals_live(t)
+
         tv = self.data[:, 0]
         i = self.index
         # i = tv.searchsorted(t).item()
@@ -82,7 +86,7 @@ class Analyzer(PredixWrap):
 
         async def root(request):
             return web.json_response(
-                self.get_vals(t=get_time()),
+                await self.get_vals(t=get_time()),
                 headers={'Access-Control-Allow-Origin': '*'})
 
         app.router.add_get('/', root)
@@ -92,18 +96,24 @@ class Analyzer(PredixWrap):
 
     async def load_data(self, t0=get_time()):
         if self.config.sim_data:
-            N = self.config.sim_time_max
+            N = self.config.get_dp_n
             v = numpy.random.randn(N).cumsum()
         else:
-            values = await self.get_datapoints(n=self.config.get_dp_n)
-            values = numpy.asarray(values)
-            v = values[:, 1]
-            N = len(values)
+            v = await self.get_values_from_predix(self.config.get_dp_n)
+            N = len(v)
 
         self.data = numpy.c_[numpy.linspace(0, self.config.sim_time_max, N),
                              v,
                              numpy.ones((N, 2))]
         # print(self.data)
+
+    async def get_values_from_predix(self, n):
+        if self.config.sim_data:
+            return numpy.random.rand(n)
+        values = await self.get_datapoints(n=n)
+        values = numpy.asarray(values)
+        v = values[:, 1]
+        return v
 
     def get_inject_idx(self):
         return self.config.inject_idx * self.config.window_len
@@ -118,21 +128,37 @@ class Analyzer(PredixWrap):
     async def compute_dq(self):
         k = self.config.window_len
         N = len(self.data)
-        kd = k // 10
+        kd = self.config.window_step
         fv = [1, 1]
         for i in range(k, N, kd):
             sl = slice(i - k, i)
             dw = self.data[sl, 1]
             try:
                 fv = self.get_feature_vector(dw)
+            except ValueError:
+                pass
+            else:
                 dq = self.get_dq(fv)
                 logger.debug('computed features: idx=%s, fv=%s, dq=%s', i, fv, dq)
 
-            except ValueError:
-                pass
-
             arr = numpy.asarray([fv] * kd)
+
+            # fill out all the missing
             self.data[i - kd: i, 2: 4] = arr
+
+    async def get_vals_live(self, t):
+        v = await self.get_values_from_predix(self.config.window_len)
+        logger.debug('got %s values' % len(v))
+        value = v[-1]
+        try:
+            fv = self.get_feature_vector(v)
+        except ValueError:
+            fv = [1, 1]
+            dq = 1
+        else:
+            dq = self.get_dq(fv)
+        return dict(time=time.time(), value=value, fv=fv, dq=dq)
+
 
 @click.command()
 @click.option('-d', '--original', is_flag=True, default=False)
